bce1dd2f6d590a2f38428beb669118d6a0e15edf
Syurkov Maksim <hijey1337@mail.ru>
2026-06-03 23:13:08 +0300

    fix auth / deploy fix + add

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://taskmanager-production-9874.up.railway.app/",
+  baseURL: "https://taskmanager-production-9874.up.railway.app/api",
 });
 
 api.interceptors.request.use((config) => {
