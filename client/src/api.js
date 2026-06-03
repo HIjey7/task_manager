794f2aa58b4d11a509ec668ecf396c5c70089155
Syurkov Maksim <hijey1337@mail.ru>
2026-06-03 21:56:07 +0300

    restore client dependencies

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://task-manager-mainv.onrender.com",
+  baseURL: "http://localhost:3000/api",
 });
 
 api.interceptors.request.use((config) => {
