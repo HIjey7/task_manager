088df34b3b171f7e1bdcf0b2936f6b27adf5149e
Syurkov Maksim <hijey1337@mail.ru>
2026-06-03 21:13:41 +0300

    build frontend + prepare production deploy

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/auth.js";
 import taskRoutes from "./routes/tasks.js";
@@ -9,26 +11,32 @@ import taskRoutes from "./routes/tasks.js";
 dotenv.config();
 
 const app = express();
-
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-  }),
-);
+app.use(cors({ origin: "*" }));
 
-const PORT = process.env.PORT || 3000;
+// ===== API =====
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
 
+// ===== MONGO =====
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("MongoDB connected");
-
-    app.use("/api/auth", authRoutes);
-    app.use("/api/tasks", taskRoutes);
-
-    app.listen(PORT, () => {
-      console.log("Server running on", PORT);
-    });
-  })
+  .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
+
+// ===== FRONTEND =====
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+// ===== START =====
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
